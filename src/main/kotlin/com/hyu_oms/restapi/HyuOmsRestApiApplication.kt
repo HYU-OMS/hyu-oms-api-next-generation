23905a5ff467c390e0f8bcfa67040bb1921462a9
Kihoon Han <me@hoony9x.com>
2020-04-30 23:23:11 +0900

    Initial files generated by 'Spring Initializr'

---
+++ src/main/kotlin/com/hyu_oms/restapi/HyuOmsRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.hyu_oms.restapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HyuOmsRestApiApplication
+
+fun main(args: Array<String>) {
+  runApplication<HyuOmsRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyu_oms/restapi/HyuOmsRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyu_oms.restapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HyuOmsRestApiApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}

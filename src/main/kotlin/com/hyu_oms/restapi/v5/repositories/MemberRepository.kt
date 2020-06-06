96456d93388568470cde025cf03ae7112aaf89af
Kihoon Han <me@hoony9x.com>
2020-06-07 02:51:51 +0900

    안 쓰는 method 제거.

--- src/main/kotlin/com/hyu_oms/restapi/v5/repositories/MemberRepository.kt
+++ src/main/kotlin/com/hyu_oms/restapi/v5/repositories/MemberRepository.kt
@@ -1,15 +1,10 @@
 package com.hyu_oms.restapi.v5.repositories
 
 import com.hyu_oms.restapi.v5.entities.Member
-import com.hyu_oms.restapi.v5.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface MemberRepository : JpaRepository<Member, Long> {
-
-
-  fun findAllByUserAndEnabledIsTrue(user: User): List<Member>
-
   fun findAllByUserIdAndEnabledIsTrue(userId: Long): List<Member>
 }

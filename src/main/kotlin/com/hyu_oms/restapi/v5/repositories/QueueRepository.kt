9b2229ba5a4aae25240194669e6553c8e8ca08da
Kihoon Han <me@hoony9x.com>
2020-06-07 02:53:20 +0900

    Repository annotation 추가.

--- src/main/kotlin/com/hyu_oms/restapi/v5/repositories/QueueRepository.kt
+++ src/main/kotlin/com/hyu_oms/restapi/v5/repositories/QueueRepository.kt
@@ -2,5 +2,7 @@ package com.hyu_oms.restapi.v5.repositories
 
 import com.hyu_oms.restapi.v5.entities.Queue
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface QueueRepository : JpaRepository<Queue, Long>
